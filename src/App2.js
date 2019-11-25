d5da3b5c06039799cfce4566e09226d5231172a2
Grégoire Maillard <gregoire.maillard@edu.ece.fr>
2019-11-25 17:48:44 +0000

    PAGES

--- src/App2.js
+++ src/App2.js
@@ -15,25 +15,19 @@ export default function App() {
       <div>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/page">Page</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
+            <Link to="/page2">Page2</Link>
           </li>
         </ul>
 
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
+          <Route path="/page2">
+            <Page2 />
           </Route>
           <Route path="/">
-            <Home />
+            <Page />
           </Route>
         </Switch>
       </div>
@@ -41,20 +35,44 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topics() {
+function Page() {
   let match = useRouteMatch();
-
   return (
     <div>
-      <h2>Topics</h2>
+      <h2>Page</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>      
+
+      {/* The Page page has its own <Switch> with more routes
+          that build on the /Page URL path. You can think of the
+          2nd <Route> here as an "index" page for all Page, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic/>
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+
+function Page2() {
+  let match = useRouteMatch();
+  return (
+    <div>
+      <h2>Page2</h2>
 
       <ul>
         <li>
@@ -67,9 +85,9 @@ function Topics() {
         </li>
       </ul>      
 
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
+      {/* The Page page has its own <Switch> with more routes
+          that build on the /Page URL path. You can think of the
+          2nd <Route> here as an "index" page for all Page, or
           the page that is shown when no topic is selected */}
       <Switch>
         <Route path={`${match.path}/:topicId`}>
