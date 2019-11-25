9bdb37c8a5287979d8976c0e983f08706f57ec2f
Grégoire Maillard <gregoire.maillard@edu.ece.fr>
2019-11-25 17:32:25 +0000

    changement page APP2

--- src/App2.js
+++ src/App2.js
@@ -1,38 +1,89 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-class App2 extends Component {
-  render() {
-    return (
-      <div className="App">
-      <p>Formulaire simple qui vous permettra d’injecter de la donnée dans votre API.</p>
-        <form>
-          <label>
-            Nom : 
-            <input type="text" name="lastname" /><br/>
-            Prenom :
-            <input type="text" name="name" /><br/>
-            Age :
-            <input type="text" name="age" /><br/>
-            Moyenne :
-            <input type="text" name="moyenne" /><br/>
-            Salaire :
-            <input type="text" name="salaire" /><br/>
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-        <a
-            href="src/App.js"
-          >PAGE 1
-          </a>
-          <br/>
-          <a
-            href="src/App2.js"           
-          >
-            PAGE 2
-          </a>
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
-export default App2;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>      
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
